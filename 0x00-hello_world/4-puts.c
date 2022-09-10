1d7dc6c491b56d7d868c6e318970737558d03728
SurafelTeka <50694227+SurafelTeka@users.noreply.github.com>
2022-09-11 02:35:17 +0800

    version 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always  success
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"
+);
+
 	return (0);
 }
